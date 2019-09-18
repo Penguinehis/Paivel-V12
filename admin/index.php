58db20a519e2f6f43fe89b84e5be0d9425d37518
HackkcaH <37765695+HackkcaH@users.noreply.github.com>
2019-09-18 10:44:47 -0300

    Update index.php

--- admin/index.php
+++ admin/index.php
@@ -118,7 +118,7 @@
     <!-- ============================================================== -->
     <!-- All Jquery -->
     <!-- ============================================================== -->
-    <script src="../../assets/plugins/jquery/jquery.min.js"></script>
+    <script src="../assets/plugins/jquery/jquery.min.js"></script>
     <!-- Bootstrap tether Core JavaScript -->
     <script src="../assets/plugins/bootstrap/js/popper.min.js"></script>
     <script src="../assets/plugins/bootstrap/js/bootstrap.min.js"></script>
